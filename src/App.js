91fc8d28f720df544f9bff8f8f95fba26dce9d44
Matias Obezzi <matiasobezzi@gmail.com>
2021-04-23 22:41:09 -0300

    Meta tags

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <HashRouter>
       <MainNav open={openMenu} toggleOpen={() => setOpenMenu(!openMenu)} />
-      <div style={{minHeight: `calc(100vh - 50px - 110px - ${window.innerWidth < 578 ? '52px' : ''})`}}>
+      <div style={{minHeight: `calc(100vh - 50px - 110px - ${window.innerWidth < 578 ? '52px' : '0'})`}}>
         <Router open={openMenu} toggleOpen={() => setOpenMenu(!openMenu)} />
       </div>
       <Footer />
